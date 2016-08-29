d1392e734b963c09d7d92eda3977953dd5886e88
k1p1l0 <s0ht@mail.ru>
2016-08-30 02:47:20 +0300

    finish tut 3

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import { createStore } from "redux";
 
 const reducer = function (state, action) {
-	return state+1;
+	if (action.type === 'INC') {
+		return state + action.payload;
+	}
+
+	if (action.type === 'DEC') {
+		return state - action.payload;
+	}
+
+	return state;
 }
 
 const store = createStore(reducer, 0);
@@ -11,3 +19,7 @@ store.subscribe(() => {
 });
 
 store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "DEC", payload: 123});
+store.dispatch({type: "INC", payload: 55});
+store.dispatch({type: "INC", payload: 51251});
+store.dispatch({type: "DEC", payload: 11123123});
